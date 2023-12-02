1169715fceee73e7e947df3c2e13ef1908e2c0b4
Vergacas <jeanvergacas@gmail.com>
2023-12-02 15:37:41 -0300

    adicionando pacotes

--- src/br/ufrn/imd/Main.java
+++ src/br/ufrn/imd/Main.java
@@ -1,4 +1,4 @@
-package application;
+package br.ufrn.imd;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,9 @@
 module MidiaPlayer {
 	requires javafx.controls;
+	requires javafx.fxml;
+	
+	exports br.ufrn.imd;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens br.ufrn.imd.controle to javafx.fxml;
 }
