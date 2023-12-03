e0d3caedf6725e29a094255fabcbc99654f97650
Vergacas <jeanvergacas@gmail.com>
2023-12-02 23:39:27 -0300

    atualizando

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
-module MidiaPlayer {
+module MidiaPlayer  {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+		
 	exports br.ufrn.imd;
-	
+		
 	opens application to javafx.graphics, javafx.fxml;
 	opens br.ufrn.imd.controle to javafx.fxml;
 }
+
