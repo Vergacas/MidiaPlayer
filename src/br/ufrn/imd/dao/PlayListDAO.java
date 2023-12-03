e63835b42d8dce52af2e3d202cce87996e16a60e
Vergacas <jeanvergacas@gmail.com>
2023-12-03 10:47:01 -0300

    Criada classe repositorio para PlayLists

--- src/br/ufrn/imd/dao/PlayListDAO.java
+++ src/br/ufrn/imd/dao/PlayListDAO.java
@@ -4,6 +4,33 @@ import br.ufrn.imd.modelo.PlayList;
 
 import java.util.ArrayList;
 
+/***
+ * Classe de armazenamento de PlayLists
+ * @author jeanv
+ * @version 1.0
+ */
 public class PlayListDAO {
 
+	private ArrayList<PlayList> pls;
+	private static PlayListDAO bdPlayList;
+	
+	
+	/***
+	 * Método construtor do PlayListDAO, que inicializa a classe.
+	 */
+	public PlayListDAO() {
+		pls = new ArrayList<PlayList>();
+	}
+	
+	public static PlayListDAO getInstance() {
+		if(bdPlayList == null) {
+			bdPlayList = new PlayListDAO();
+		}
+		
+		return bdPlayList;
+	}
+	
+	public void addPlayList(PlayList pl) {
+		pls.add(pl);
+	}
 }
