b8ef4c82313f81ffa0bce6d2d17497a9e317f7dc
Vergacas <jeanvergacas@gmail.com>
2023-12-03 00:27:04 -0300

    criada classe de armazenamento para musica

---
+++ src/br/ufrn/imd/dao/MusicaDAO.java
@@ -0,0 +1,63 @@
+package br.ufrn.imd.dao;
+
+import br.ufrn.imd.modelo.Musica;
+
+import java.util.ArrayList;
+
+/***
+ * Essa é a classe de armazenamento de Usuario.
+ * 
+ * @author jeanv
+ * @version 1.0
+ */
+public class MusicaDAO {
+
+	private ArrayList<Musica> musicas;
+	private static MusicaDAO bdMusica;
+	
+	/***
+	 * Método costrutor de MusicaDAO que inicializa a classe.
+	 * 
+	 */
+	public MusicaDAO() {
+		musicas = new ArrayList<Musica>();
+	}
+	
+	public static MusicaDAO getIsntace() {
+		if(bdMusica == null) {
+			bdMusica = new MusicaDAO();
+		}
+		return bdMusica;
+	}
+	
+	/***
+	 * Método addMusica que adiciona uma musica a lista de musicas
+	 * @param m
+	 */
+	public void addMusica(Musica m) {
+		Integer id = musicas.size();
+		m.setId(id);
+		musicas.add(m);
+	}
+	
+	/***
+	 * Método removerMusica que remove uma musica da lista de musicas
+	 * @param m
+	 */
+	public void removerMusica(Musica m) {
+		musicas.remove(m);
+	}
+	
+	/***
+	 * método para listar as musicas
+	 * @return output
+	 */
+	public String listarMusicas() {
+		String output = "-----------------------------------" + "\n";
+		for(Musica m : musicas) {
+			output += "Nome: " + m.getNome() + "\tAutor: " + m.getAutor() + "\n";
+			output += "-----------------------------------" + "\n";
+		}
+		return output;
+	}
+}
